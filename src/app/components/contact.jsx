4c71c6b67265d5709b4b87561cce55797b874f93
ohaib-cod <alizohaib.web@gmail.com>
2026-09-22 16:09:09 +0500

    try colors

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -19,6 +19,7 @@ function Contact() {
         <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-transparent focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
       </div>
       <button className="text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">Subscribe</button>
+     {/* so this is dummy website */}
     </div>
   </div>
 </section>
